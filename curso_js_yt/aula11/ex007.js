3827c83bf758f1bb135b42aa744dbccfbf9a59dc
Lucas <lucasverzani90@gmail.com>
2022-01-19 23:14:26 -0300

    fazendo pafinas interativas com condições

---
+++ curso_js_yt/aula11/ex007.js
@@ -0,0 +1,6 @@
+var vel = 32
+console.log(`Sua velocidade foi ${vel} km/k`)
+if(vel > 60){
+    console.log(`Você passou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
---
+++ curso_js_yt/aula11/ex008.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`você mora no ${pais}`)
+if(pais != Brasil) {
+    console.log(`Você é Estrangeiro!`)
+}else{
+    console.log(`Você é Brasileiro!`)
+}
