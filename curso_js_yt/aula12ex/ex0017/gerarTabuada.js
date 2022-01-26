944eb2b43f67a9a649731c77dcc0063e60c76ae1
conlicitacao <conlicitacao@MacBook-Pro-de-conlicitacao.local>
2022-01-26 08:37:47 -0300

    feat(service): inclide new js to calculate numbers

--- curso_js_yt/aula12ex/ex0017/gerarTabuada.js
+++ curso_js_yt/aula12ex/ex0017/gerarTabuada.js
@@ -1,3 +1,9 @@
 function tabuada(){
-    alert(ola)
+    let num = document.getElementById('txtnum')
+    let tab = document.getElementById('tab  ')
+    if(num.value.length === 0) {
+        alert("por favor, digite um numero")        
+    }else{
+        let n = Number(num.value)
+    }
 }
