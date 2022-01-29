e407917bb8a94f2c29589555dcf6b78258e574af
Lucas <lucasverzani90@gmail.com>
2022-01-29 17:06:50 -0300

    conslusão de desafios em js, listas aninhadas e tabelas em HTML

---
+++ curso_Js_Udemy/aula01/script.js
@@ -0,0 +1,13 @@
+let valor1 = 5;
+let valor2 = 6;
+let valor3 = "9";
+
+
+function somarValores(n1, n2) {
+    let num = n1 + n2
+    console.log(num);
+}
+
+somarValores(valor1, valor2);
+somarValores(valor2, valor3);
+somarValores("200", valor3);
---
+++ curso_Js_Udemy/aula02/script.js
@@ -0,0 +1,12 @@
+let vetor = [1, 3, 5,  7,  9, 11, 13, ]
+
+let numeros = vetor[1] + vetor[4];
+
+console.log(numeros > 9);
+console.log('------');
+console.log((vetor[6] - numeros) == 5 || true);
+console.log('------');
+console.log(true == "true");
+console.log('------');
+console.log(true === "true");
+console.log('------')
